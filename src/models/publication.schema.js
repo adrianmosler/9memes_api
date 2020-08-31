a17d9031f89ff1e14f9d205647e49b9849910478
MCele <celeste.ramos@fi.uncoma.edu.ar>
2020-08-31 20:54:07 -0300

    se agrega dependencias y rutas

---
+++ src/models/publication.schema.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import usuarioSchema from "./user.schema";
+
+const Schema = mongoose.Schema;
+
+export let publicationSchema = new Schema({
+  title: { type: String, required: true },
+  description: {
+    type: String,
+    required: true,
+  },
+  likes: [usuarioSchema],
+  unLikes: [usuarioSchema],
+  category: [categorySchema],
+  img: {
+    type: String,
+    required: true,
+  },
+  createdAt: Date,
+  createdBy: usuarioSchema,
+});
+
+module.exports = mongoose.model("publication", PublicationSchema);
